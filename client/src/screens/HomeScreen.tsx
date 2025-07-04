2b9a037cfcec893b04c789a6d1d9f1fbce08d1b1
sonseongyeol <sonseong10@gmail.com>
2025-07-04 14:33:39 +0900

    Study bottom tool bar edit

---
+++ client/src/screens/HomeScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>홈 화면입니다!</Text>
+    </View>
+  );
+}
